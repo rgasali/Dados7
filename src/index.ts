e191738c3569ff2a590ae4a22a7e6f5895b3df90
rgasali <96292535+rgasali@users.noreply.github.com>
2022-06-16 11:21:10 -0300

    html-y-css se agrego estilo y se le muestra el resultado en el html.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
 "use strict";
-
-let contadorSeSietes: number = 0;
+let btnArrojarDados = <HTMLButtonElement>(
+  document.getElementById("btnArrojarDados")
+);
+let divRespuesta = <HTMLDivElement>document.getElementById("divRespuesta");
+let contadorDeSietes: number = 0;
 
 function lanzarDados() {
   for (let i = 0; i < 1000; i++) {
     let lanzamiento: number = 0;
     lanzamiento = Math.floor(Math.random() * (7 - 1) + 1);
     lanzamiento = lanzamiento + Math.floor(Math.random() * (7 - 1) + 1);
-    console.log(lanzamiento);
     if (lanzamiento === 7) {
-      contadorSeSietes++;
+      contadorDeSietes++;
     }
   }
 }
 
-lanzarDados();
-console.log("la cantidad de veces que salio 7 es");
-console.log(contadorSeSietes);
+btnArrojarDados.addEventListener("click", () => {
+  lanzarDados();
+  divRespuesta.innerHTML = String(
+    `la cantidad de veces que salio 7 es ${contadorDeSietes}`
+  );
+  contadorDeSietes = 0;
+});
